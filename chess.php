aabc62a77dda7f611f5d0032d3ab90c299da16a5
Rishit Srivastava <87407052+Notproffesionalcoder@users.noreply.github.com>
2021-08-02 17:58:28 +0530

    Create chess.php

---
+++ chess.php
@@ -0,0 +1,22 @@
+<table border = '2'>
+<?php
+for($i=1; $i<9;$i++)
+{
+    ?> <tr> <?php
+    for($j=1; $j<9; $j++)
+    {
+        $total = $i+$j;
+        if($total%2 == 0)
+        {
+            ?> <td style="background-color: black;" width = 80px; height = 80px;> </td><?php
+        }
+        else
+        {
+            ?> <td width = 80px; height = 80px;> </td><?php
+        }
+        
+    }
+    ?> </tr> <?php
+}
+?>
+</table>
